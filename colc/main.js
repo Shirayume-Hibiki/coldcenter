ff2f28b09dcab0609608bda740ab07a8f87a3147
Shirayume-Hibiki <109019974+Shirayume-Hibiki@users.noreply.github.com>
2024-09-01 21:26:56 +0700

    colc folder upload

---
+++ colc/main.js
@@ -0,0 +1,7 @@
+import '@maptiler/sdk/dist/maptiler-sdk.css';
+import * as maptilersdk from '@maptiler/sdk';
+
+const key = 'hMTdUv602spvwVDfuykt';
+maptilersdk.config.apiKey = key;
+
+// The rest of the JavaScript code is handled in the HTML's script block
