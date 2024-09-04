e818c387edbbb6834ec6623c5c08ae80e4131b1d
Shirayume-Hibiki <109019974+Shirayume-Hibiki@users.noreply.github.com>
2024-09-04 10:03:50 +0700

    Delete colc directory, replacing

--- colc/main.js
+++
@@ -1,7 +0,0 @@
-import '@maptiler/sdk/dist/maptiler-sdk.css';
-import * as maptilersdk from '@maptiler/sdk';
-
-const key = 'hMTdUv602spvwVDfuykt';
-maptilersdk.config.apiKey = key;
-
-// The rest of the JavaScript code is handled in the HTML's script block
